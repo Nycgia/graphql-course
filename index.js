841e3dd92b21728535a82ad498b213a4a023230e
Nycgia <pmunozb@protonmail.com>
2020-07-12 15:57:09 -0500

    Resolvers is added

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const { graphql, buildSchema } = require('graphql');
 // Define schema
 const schema = buildSchema(`
     type Query {
-        hello: String
+        hello: String,
+        saludo: String
     }
 `);
 
+// Configurate the "resolvers"
+const resolvers = {
+    hello: () => 'Hola Mundo',
+    saludo: () => 'Peter choro mota'
+};
+
 // Execute the query
-graphql(schema, `{ hello }`)
+graphql(schema, `{ hello, saludo }`, resolvers)
     .then(data => {
         console.log(data)
     })
