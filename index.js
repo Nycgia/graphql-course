6f3131b550d587f82e4631a4eeefe47234c1c971
Nycgia <pmunozb@protonmail.com>
2020-07-12 16:24:27 -0500

    Schema file and resourses file is added in lib directory

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql');
+const { buildSchema } = require('graphql')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
 
 // Define schema
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-        saludo: String
-    }
-`);
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
-// Configurate the "resolvers"
-const resolvers = {
-    hello: () => 'Hola Mundo',
-    saludo: () => 'Peter choro mota'
-};
+app.use('/api', graphqlHTTP({
+  schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
 
-// Execute the query
-graphql(schema, `{ hello, saludo }`, resolvers)
-    .then(data => {
-        console.log(data)
-    })
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
